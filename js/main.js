c2bf6e698891ba4bdef174038f12b7e1ba7dd1ef
Roman Dubovik <ip.romandubovik@gmail.com>
2020-11-03 18:19:36 +0300

    on some stage...still working

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openSideMenu() {
+  document.getElementById('js--side-menu').style.width = '20vw';
+}
+
+function closeSideMenu() {
+  document.getElementById('js--side-menu').style.width = '0';
+}
