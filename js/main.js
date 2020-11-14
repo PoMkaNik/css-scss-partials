808942e5dc40a3ab1a050b9fa8aa53da1a74cc2d
Roman Dubovik <ip.romandubovik@gmail.com>
2020-11-14 19:18:32 +0300

    signIn-signOut section added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// nav-main
 function openSideMenu() {
   document.getElementById('js--side-menu').style.width = '20vw';
 }
@@ -5,3 +6,15 @@ function openSideMenu() {
 function closeSideMenu() {
   document.getElementById('js--side-menu').style.width = '0';
 }
+
+// sign-in <-> sign-out
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () =>
+  container.classList.add('right-panel-active'),
+);
+signInButton.addEventListener('click', () =>
+  container.classList.remove('right-panel-active'),
+);
